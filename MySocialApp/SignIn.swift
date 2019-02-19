401bfdab3ef73ad10fa212feb4a6ecf826785de1
Khaled Bohout <amrutaka@MacBook-Pro-alkhas-b-amr.local>
2019-02-19 20:57:45 +0200

    firebase-setup-completed

---
+++ MySocialApp/SignIn.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySocialApp
+//
+//  Created by Khaled Bohout on 2/19/19.
+//  Copyright © 2019 Khaled Bohout. All rights reserved.
+//
+
+import UIKit
+
+class SignIn: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
