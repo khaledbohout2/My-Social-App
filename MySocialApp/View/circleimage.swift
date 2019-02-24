144ad815c387dfe319230ef3f1f17195c6530870
Khaled Bohout <amrutaka@MacBook-Pro-alkhas-b-amr.local>
2019-02-24 18:21:39 +0200

    feed ui completed

--- MySocialApp/View/circleimage.swift
+++ MySocialApp/View/circleimage.swift
@@ -1,23 +1,30 @@
 //
-//  FancyView.swift
-//  MySocial
+//  circleimage.swift
+//  MySocialApp
 //
-//  Created by Khaled Bohout on 2/19/19.
+//  Created by Khaled Bohout on 2/24/19.
 //  Copyright © 2019 Khaled Bohout. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class circleimage: UIImageView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         layer.shadowColor = UIColor(red: shadow_grey, green: shadow_grey, blue: shadow_grey, alpha: 0.6).cgColor
-        layer.shadowOpacity = 0.0
+        layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func layoutSubviews() {
+        
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+        
+
     }
 
 
